1fb6556edc9c15b89758615670d75c38e997a9c0
rajammajetti <rajammajetti737@gmail.com>
2026-05-15 16:42:22 +0530

    Update SpringbootTomcatJenkinsApplication.java

--- src/main/java/com/ibm/SpringbootTomcatJenkinsApplication.java
+++ src/main/java/com/ibm/SpringbootTomcatJenkinsApplication.java
@@ -1,19 +1,13 @@
-package com.example;
+package com.ibm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootHelloApplication extends SpringBootServletInitializer {
+public class SpringbootTomcatJenkinsApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringbootHelloApplication.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTomcatJenkinsApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootHelloApplication.class, args);
-    }
 }
